2279e4bd53145e92c0d963d1cf22945bc4693d19
Rj Allen <rjallen@2015-macos-1013.local>
2021-02-19 23:15:12 -0500

    old files

--- project/main.py
+++ project/main.py
@@ -3,13 +3,15 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
-@main.route('/')
+
+@main.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@main.route('/profile')
+
+@main.route("/profile")
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    return render_template("profile.html", name=current_user.name)
