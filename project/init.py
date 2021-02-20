5f3e10ba7351e3bea55274d067fda34fa5ef01a7
Rocky Allen <rallen@dynatronsotware.com>
2021-02-20 03:13:58 -0500

    adding the linebreaks

--- project/init.py
+++ project/init.py
@@ -16,14 +16,11 @@ def create_app():
     app.config[
         "SQLALCHEMY_DATABASE_URI"
     ] = "postgresql+psycopg2://postgres:passw0rd@localhost:5432/postgres"
-
     db.init_app(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
-
-    from .models import Users
 
 
 password = b"passw0rd"
@@ -33,13 +30,17 @@ pin = b"9493910663"
 db = create_engine(db_string)
 metadata = MetaData()
 
+"""
+with store_context(store):
+    Blob = user.picture.make_blob()
+"""
 print(nacl.pwhash.argon2id.str(password))
 print(nacl.pwhash.argon2id.str(pin))
 
 Users = Table(
-    "users",
+    "Users",
     metadata,
-    Column("id", BigInteger, Sequence("user_id_seq"), primary_key=True),
+    Column("id", BigInteger, Sequence("user_id_seq0"), primary_key=True),
     Column("name", String, unique=True),
     Column("PIN", String, nullable=False),
     Column("pgp_public_key", Text),
@@ -47,20 +48,19 @@ Users = Table(
     Column("is_vendor", SmallInteger, default=0),
     Column("bip32_key", Text),
     Column("created", DateTime, nullable=False, default=func.now()),
-    Column("Modified", DateTime, nullable=False, onupdate=func.utc_times
+    Column("Modified", DateTime, nullable=False, onupdate=func.utc_times()),
+)
+
 Products = Table(
     "Products",
     metadata,
     Column("id", BigInteger, nullable=True, primary_key=True),
-    Column("name", String, nullable=True),
+    Column("name", String(255), nullable=True),
     Column("description", Text, nullable=True),
-    Column("price", F                                                                                                                                                                                                                                                                                                                                                           vb                                                       loat, nullable=True),
-    Column("tags", Text, nullable=True),
+    Column("price", Numeric, nullable=False),
     Column("is_hidden", SmallInteger, nullable=True, default=0),
-                                                                                   0
-
     Column("code", String, nullable=True),
-    Column("image", Blob, nullable=True),
+    # Column("image", Blob, nullable=True),
     Column("category", BigInteger, nullable=True),
 )
 
@@ -69,12 +69,13 @@ class User(object):
     def __init__(
         self, name, PIN, password, pgp_public_key, is_vendor, bip32_key, bip32_key_index
     ):
-    ][[='[=========================================================                                                                                                                                                                                                                                                                                                                                                                                                                                                            ]']]
+        self.name = name
         self.PIN = PIN
         self.password = password
         self.pgp_public_key = pgp_public_key
+        self.is_vendor = is_vendor
         self.bip32_key = bip32_key
-        sis_vendor
+        self.bip32_key_index = bip32_key_index
 
 
 class Products(object):
@@ -88,11 +89,11 @@ class Products(object):
         self.tags = tags
         self.is_hidden = is_hidden
         self.code = code
-        self.image = image
+        #    self.image = image
         self.category = category
 
 
-mapper(User, users)
+mapper(User, Users)
 mapper(Products, Products)
 
 metadata.create_all(db)
